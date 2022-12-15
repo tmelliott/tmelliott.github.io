553535784e9bbf4d672c73f020ad8847f95c916a
Tom Elliott <tomelliottnz@gmail.com>
2022-12-15 17:37:04 +1300

    some content and theme

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,16 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Tom Elliott";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_DESCRIPTION =
+  "Data science, visualisation, and computing mixed with wine, gardening, food, and music.";
+
+export const SITE_LINKS = [
+  {
+    title: "Blog",
+    url: "/blog",
+  },
+  {
+    title: "Outputs",
+    url: "/outputs",
+  },
+];
