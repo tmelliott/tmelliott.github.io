bf340bbd5fb5de85ec8f72376e7e81cd2931c1ea
Tom Elliott <tomelliottnz@gmail.com>
2022-12-28 09:21:30 +1300

    tidy up responsiveness

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   const today = new Date();
   return (
-    <footer className="pt-4 pb-8 text-white px-4 text-sm">
+    <footer className="pt-4 pb-8 text-white px-4 text-sm max-w-2xl mx-auto w-full">
       &copy; {today.getFullYear()} Tom Elliott. All rights reserved.
     </footer>
   );
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,6 +3,8 @@ import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
+import profilePic from "../../../public/photo.jpg";
+
 export const SITE_LINKS = [
   {
     title: "Blog",
@@ -23,14 +25,15 @@ export default function Header() {
         <div className="bg-gray-50 flex flex-col items-center justify-center mt-[30vh] relative z-20">
           <Link
             href="/"
-            className="absolute rounded-full -translate-y-1/2 top-0 z-20 border-8 border-gray-50"
+            className="absolute rounded-full -translate-y-1/2 top-0 z-20 border-8 border-gray-50 bg-gray-50 h-[180px] w-[180px]"
           >
             <Image
-              src="/photo.jpg"
+              src={profilePic}
               className="rounded-full z-30"
               width={180}
               height={180}
               alt="Tom Elliott"
+              placeholder="blur"
             />
           </Link>
           <h2 className="font-bold mt-[100px] pt-2 text-2xl tracking-wide">
@@ -60,20 +63,19 @@ export default function Header() {
   }
 
   return (
-    <header className="h-[50px] mt-[50px] bg-gray-50">
-      <div className="fixed z-30 bg-slate-800 top-0 w-full h-[50px]">
-        <div className="fixed z-30 top-[50px] w-full flex flex-col justify-end h-[50px] bg-opacity-80 bg-gradient-to-b from-slate-50 via-slate-50 to-transparent">
+    <header className="relative h-[50px] mt-[50px] bg-gray-50">
+      <div className="absolute z-30 top-0 w-full h-[50px]">
+        <div className="z-30 top-[50px] w-full flex flex-col justify-end h-[50px] bg-opacity-80 bg-gradient-to-b from-slate-50 via-slate-50 to-transparent max-w-3xl mx-auto">
           <div className="absolute top-0 w-full flex items-center justify-start gap-6 px-4 h-[100px] z-50 -translate-y-1/2">
             <Link
               href="/"
-              className="rounded-full z-20 border-4 border-gray-50"
+              className="rounded-full z-20 border-4 border-gray-50 bg-gray-50 h-[60px] w-[60px]"
             >
               <Image
-                src="/photo.jpg"
-                className="rounded-full z-30"
-                width={60}
-                height={60}
+                src={profilePic}
                 alt="Tom Elliott"
+                className="rounded-full z-30"
+                placeholder="blur"
               />
             </Link>
 
