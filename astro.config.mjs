4dad108844204fe2101683c2c8c36da8594b3ada
Tom Elliott <tomelliottnz@gmail.com>
2024-01-07 12:32:11 +1300

    add sharp

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 
@@ -6,5 +6,5 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), icon()]
+  integrations: [tailwind(), mdx(), icon()],
 });
