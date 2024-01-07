b26488550a5ca2db27dcd003c9365c465f634ec8
Tom Elliott <tomelliottnz@gmail.com>
2024-01-07 13:59:36 +1300

    use astro-imagetools for placeholder

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
+import { astroImageTools } from "astro-imagetools";
 
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), icon()],
+  integrations: [tailwind(), mdx(), icon(), astroImageTools],
   site: "https://tomelliott.co.nz",
 });
